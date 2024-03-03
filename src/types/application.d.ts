b225396e2beae1c9d10d6687465e67300b36d6a2
skyflyhighly <skyflyhighly@gmail.com>
2024-03-04 01:30:22 +0700

    feat (DataProvider): Add filtered clients

--- src/types/application.d.ts
+++ src/types/application.d.ts
@@ -1,11 +1,12 @@
 interface IClient {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phoneNumber: string;
+	id: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+	phoneNumber: string;
 }
 
 interface IApplicationState {
-  clients: IClient[];
+	clients: IClient[];
+	filteredClients: IClient[];
 }
