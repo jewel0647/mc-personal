a352e7b42214f1ff3d6d801fcf3f5577c1ea1f90
skyflyhighly <skyflyhighly@gmail.com>
2024-03-04 01:30:40 +0700

    feat (Clients): Add search functionality

--- src/pages/Clients/index.tsx
+++ src/pages/Clients/index.tsx
@@ -1,25 +1,53 @@
 import { memo, useContext, useEffect } from 'react';
-import { Paper, Typography } from '@mui/material';
+import { Paper, Typography, Stack } from '@mui/material';
 import { StateContext } from '../../store/DataProvider';
 import Page from '../../components/Page';
 import ClientTable from '../../components/pages/clients/ClientTable';
+import Search from '../../components/basic/search/Search';
+import CustomButton from '../../components/basic/buttons/CustomButton';
 import { getClients } from '../../services/api';
 
 function Clients() {
 	const { state, dispatch } = useContext(StateContext);
-	const { clients } = state;
+	const { clients, filteredClients } = state;
 
 	useEffect(() => {
-		getClients().then((clients) => dispatch({ type: 'FETCH_ALL_CLIENTS', data: clients }));
+		getClients().then((clients) => {
+			dispatch({ type: 'FETCH_ALL_CLIENTS', data: clients });
+			dispatch({ type: 'SEARCH_CLIENTS', data: clients });
+		});
 	}, [dispatch]);
+
+	const handleClientSearch = (searchKeyword: string) => {
+		if (!searchKeyword) {
+			dispatch({ type: 'SEARCH_CLIENTS', data: clients });
+			return;
+		}
+		dispatch({
+			type: 'SEARCH_CLIENTS',
+			data: clients.filter((client) => {
+				return (client.firstName + client.lastName).toLowerCase().includes(searchKeyword.toLowerCase());
+			}),
+		});
+	};
 
 	return (
 		<Page>
 			<Typography variant='h4' sx={{ textAlign: 'start' }}>
 				Clients
 			</Typography>
-			<Paper sx={{ margin: 'auto', marginTop: 3 }}>
-				<ClientTable clients={clients} />
+			<Stack
+				sx={{ marginTop: 3 }}
+				direction='row'
+				alignItems='center'
+				justifyContent='space-between'
+				spacing={{ xs: 1, sm: 2 }}
+			>
+				<Search placeholder='Search clients...' onSearch={handleClientSearch} />
+				<CustomButton label='Create new client' size='large' />
+			</Stack>
+			<Paper sx={{ margin: 'auto', marginTop: 2 }}>
+				<ClientTable clients={filteredClients} />
 			</Paper>
 		</Page>
 	);
