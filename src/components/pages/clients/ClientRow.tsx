50ed5f7a68d54c2ed48daa92cac73678fceeb88b
skyflyhighly <skyflyhighly@gmail.com>
2024-03-04 16:01:32 +0700

    style (client): Update the client table - Update the client table style

--- src/components/pages/clients/ClientRow.tsx
+++ src/components/pages/clients/ClientRow.tsx
@@ -1,28 +1,29 @@
-import { TableCell, TableRow } from "@mui/material";
+import { TableCell, TableRow } from '@mui/material';
+import { blue } from '@mui/material/colors';
 
 export interface IProps {
-  client: IClient;
+	client: IClient;
 }
 
 export default function ClientListItem({ client }: IProps) {
-  const { id, firstName, lastName, email, phoneNumber } = client;
+	const { id, firstName, lastName, email, phoneNumber } = client;
 
-  return (
-    <TableRow
-      key={id}
-      sx={{
-        "&:last-child td, &:last-child th": { border: 0 },
-        cursor: "pointer",
-        "&:hover": {
-          backgroundColor: "#f5f5f5",
-        },
-      }}
-    >
-      <TableCell component="th" scope="row">
-        {firstName} {lastName}
-      </TableCell>
-      <TableCell>{phoneNumber}</TableCell>
-      <TableCell>{email}</TableCell>
-    </TableRow>
-  );
+	return (
+		<TableRow
+			key={id}
+			sx={{
+				'&:last-child td, &:last-child th': { border: 0 },
+				cursor: 'pointer',
+				'&:hover': {
+					backgroundColor: '#f5f5f5',
+				},
+			}}
+		>
+			<TableCell component='th' scope='row' sx={{ color: blue[500], fontWeight: '600' }}>
+				{firstName} {lastName}
+			</TableCell>
+			<TableCell>{phoneNumber}</TableCell>
+			<TableCell>{email}</TableCell>
+		</TableRow>
+	);
 }
